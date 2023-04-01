c53454b9f8b971a1bef63fc650fc1e4b2449ea77
kd <kd@strife>
2023-04-01 14:49:10 +0200

    basic configuration of alacritty

--- bin/systemSetup.sh
+++ bin/systemSetup.sh
@@ -12,7 +12,7 @@ function check_installation {
 	exit 1
 }
 
-BASIC_TOOLS=(unzip curl fc-cache alacritty)
+BASIC_TOOLS=(unzip curl fc-cache alacritty tmux)
 for i in "${BASIC_TOOLS[@]}"
 do
 	check_installation "$i"
@@ -25,7 +25,7 @@ NERDFONT_ZIP_FILE="JetBrainsMono.zip"
 NERDFONT_DOWNLOAD_LINK="https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/$NERDFONT_ZIP_FILE"
 LOCAL_FONT_DIR="$HOME/.local/share/fonts"
 [ ! -f "$LOCAL_FONT_DIR/$NERDFONT_TEST_FILE" ] && (
-
+	echo "installing nerdfonts"
 	mkdir -p "$LOCAL_FONT_DIR"
 	mkdir -p /tmp/installfonts
 	cd /tmp/installfonts
@@ -33,5 +33,14 @@ LOCAL_FONT_DIR="$HOME/.local/share/fonts"
 	unzip $NERDFONT_ZIP_FILE -d "$LOCAL_FONT_DIR"
        	rm -rf /tmp/installfonts
 	fc-cache -f -v
+)
+
+# install alacritty themes
+ALACRITTY_THEMES_DIR="$HOME/.config/alacritty/themes"
+ALACRITTY_THEMES_REPO="https://github.com/rajasegar/alacritty-themes.git"
+[ ! -d "$ALACRITTY_THEMES_DIR" ] && (
+	echo "installing alacritty themes"
+	mkdir -p "$ALACRITTY_THEMES_DIR"
+	git clone "$ALACRITTY_THEMES_REPO" "$ALACRITTY_THEMES_DIR"
 )
 
