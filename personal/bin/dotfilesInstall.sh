26ce422cfd07e6e876af6f58113c22e1a30c466b
kd <kd@strife>
2023-05-06 17:03:54 +0200

    fix dotfiles install script

--- personal/bin/dotfilesInstall.sh
+++ personal/bin/dotfilesInstall.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+## usage:
+#
+#	curl https://raw.githubusercontent.com/luckyhive/dotfiles/master/personal/bin/dotfilesInstall.sh | bash
+
 DOTFILES_PARENT=$HOME/personal/git
 [ ! -d "$DOTFILES_PARENT/dotfiles" ] && (
 	mkdir -p $DOTFILES_PARENT
 	git clone --bare https://github.com/luckyhive/dotfiles.git $DOTFILES_PARENT/dotfiles
 	cd $HOME
-	dotfiles checkout
-	dotfiles config --local status.showUntrackedFiles no
+	git --git-dir=$HOME/personal/git/dotfiles/ --work-tree=$HOME checkout
+	git --git-dir=$HOME/personal/git/dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
 )
 
